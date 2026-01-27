f57269e62c7000aaa6e31f8cc3438497b3a0ceb0
Egor <egor@MacBook-Air-Egor.local>
2026-01-27 21:33:48 +0300

    Добавил главное меню и его обработку через (switch), не работает удаление пилотов

--- hello.cpp
+++ hello.cpp
@@ -6,12 +6,26 @@
 #include <filesystem>
 #include <ctime>
 
+
 namespace fs = std::filesystem;
 using namespace std;
 int MAX_CHARACTERS_NAME = 20;
 string FORBIDEN_CHARACTERS = "!?,.%:;'&#@";
 int choice = 1;
+string folder_path = "/Users/egor/Documents/1 Project/Pilots";
+string file_path = "/Users/egor/Documents/1 Project/Pilots/pilots.txt";
 
+void list(){
+    int c = 1;
+    string line;
+    ifstream list_file(file_path);
+    if (list_file.is_open()){
+        while(getline(list_file, line)){
+            cout << c << ")" << line << endl;
+            c++;
+        }
+    }
+}
 void logEvent(string importance, string message){
     time_t result = time(NULL);
     string time_str = ctime(&result);
@@ -38,15 +52,14 @@ bool validate(const string& text){
 }
 
 void writen(const string& name){
-    string folder_path = "/Users/egor/Documents/1 Project/Pilots";
-    string file_path = "/Users/egor/Documents/1 Project/Pilots/pilots.txt";
+    
     fs::create_directory(folder_path);
 
     ofstream file(file_path, ios::app);
     file << name << endl;
     file.close();
 }
-void next_choice(){
+void next_user(){
     string answer;
     cout << "Добавляем нового пользователя?" << endl;
     getline(cin, answer);
@@ -64,28 +77,76 @@ void next_choice(){
         throw runtime_error("Принимаем только 'Да/Нет'");
     }
 }
-
+void back_to_menu(){
+    string answer;
+    cout << "Вернуться в главное меню?" << endl;
+    getline(cin, answer);
+    try{
+        if(answer == "Да" || answer == "да"){
+            choice = 1;
+            logEvent("INFO", "Открытие главного меню");
+        }
+        else if(answer == "Нет" || answer == "нет"){
+            choice = 0;
+            logEvent("INFO", "Завершение программы");
+        }
+    }
+    catch(const exception& e){
+        throw runtime_error("Принимаем только 'Да/Нет'");
+    }
+}
 int main(){
 //Интерактив
     while (choice == 1){
+        int point;
+        string str_point;
         string name;
-        cout << "Введите имя (QUIT — выход из программы): " << endl;
-        getline(cin, name);
-        if (name == "QUIT"){
-            logEvent("INFO", "Завершение программы");
-            break;
+        cout << "WELCOME TO TXC SYSTEM" << endl;
+        cout << "1. Вывести список пилотов " << endl << "2. Добавить пилота " << endl << "3. Удалить пилота " << endl << "4.Выход " << endl;
+        
+        try{ 
+            getline(cin, str_point);
+            point = stoi(str_point);
+            switch(point)
+            {
+            case 1:
+                list();
+                back_to_menu();
+                break;
+            case 2:
+                while(true){
+                    cout << "Введите имя: " << endl;
+                    getline(cin, name);
+                    try{
+                        if (validate(name)){
+                            writen(name);
+                            logEvent("INFO", "Новый пилот добавлен в реестр.");
+                            cout << "Система приветствует, " << name << ". Инициализация протокола 'СП ТХС'" << endl;
+                            next_user();
+                            break;
+                        }
+                    }
+                catch(const exception& e){
+                    cerr << "Ошибка! " << e.what() << endl;
+                }
+                }
+                
+                
+            case 4:
+                logEvent("INFO", "Завершение программы");
+                choice = 0;
+                break;
+
+            default:
+                logEvent("ERROR", "Обнаружение неизвестной ошибки!!!");
+                cerr << "Неизвестная ошибка!" << endl;
         }
-        try{
-            if (validate(name)){
-                writen(name);
-                logEvent("INFO", "Новый пилот добавлен в реестр.");
-                cout << "Система приветствует, " << name << ". Инициализация протокола 'СП ТХС'" << endl;
-                next_choice();
-            }
+        
         }
-        catch(const exception& e){
-            cerr << "Ошибка! " << e.what() << endl;
+        catch(...){
+            cerr << "Ошибка! Неправильное действие!" << endl;
         }
+        
     }
     return 0;
 }
