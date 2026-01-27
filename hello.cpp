82adcd53623b9286e6470825df41375d72dc0313
Egor <egor@MacBook-Air-Egor.local>
2026-01-27 19:57:15 +0300

    Добавил логи

--- hello.cpp
+++ hello.cpp
@@ -4,6 +4,7 @@
 #include <fstream>
 #include <stdexcept>
 #include <filesystem>
+#include <ctime>
 
 namespace fs = std::filesystem;
 using namespace std;
@@ -11,15 +12,27 @@ int MAX_CHARACTERS_NAME = 20;
 string FORBIDEN_CHARACTERS = "!?,.%:;'&#@";
 int choice = 1;
 
+void logEvent(string importance, string message){
+    time_t result = time(NULL);
+    string time_str = ctime(&result);
+    ofstream file("/Users/egor/Documents/1 Project/logs.txt", ios::app);
+    if (file.is_open()){
+        file << "[" << time_str << "]" << "[" << importance << "] " << message << endl;
+        file.close();
+    }  
+}
 bool validate(const string& text){
     if (text.length() > MAX_CHARACTERS_NAME){
-        throw runtime_error("Длина имени не должна превышать 20 символов!" );
+        logEvent("ERROR", "Введена слишком большая длина имени!");
+        throw runtime_error("Длина имени не должна превышать 20 символов!" ); 
     }
     if(text.empty()){
+        logEvent("ERROR", "Введено постое имя!");
         throw runtime_error("Имя не может быть пустое, ты тупой?");
     }
     if(text.find_first_of(FORBIDEN_CHARACTERS) != string::npos){
-        throw runtime_error("Убери специальные символы из имени!" );
+        logEvent("ERROR", "Введены специальные (запрещенные) символы!");
+        throw runtime_error("Убери специальные символы из имени!"); 
     }
     return true;
 }
@@ -30,9 +43,6 @@ void writen(const string& name){
     fs::create_directory(folder_path);
 
     ofstream file(file_path, ios::app);
-    if (!file.is_open()){
-        throw runtime_error("Файл уже открыт");
-    }
     file << name << endl;
     file.close();
 }
@@ -43,9 +53,11 @@ void next_choice(){
     try{
         if(answer == "Да" || answer == "да"){
             choice = 1;
+            logEvent("INFO", "Новый процесс добавления пилота");
         }
         else if(answer == "Нет" || answer == "нет"){
             choice = 0;
+            logEvent("INFO", "Завершение программы");
         }
     }
     catch(const exception& e){
@@ -60,11 +72,13 @@ int main(){
         cout << "Введите имя (QUIT — выход из программы): " << endl;
         getline(cin, name);
         if (name == "QUIT"){
+            logEvent("INFO", "Завершение программы");
             break;
         }
         try{
             if (validate(name)){
                 writen(name);
+                logEvent("INFO", "Новый пилот добавлен в реестр.");
                 cout << "Система приветствует, " << name << ". Инициализация протокола 'СП ТХС'" << endl;
                 next_choice();
             }
